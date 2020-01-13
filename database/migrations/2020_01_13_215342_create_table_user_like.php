f5bad630aa78475511d30205bba270906ee7bbe6
Anderson <anderson.ivan.fuentes@gmail.com>
2020-01-13 16:30:51 -0600

    Create user_like migration

---
+++ database/migrations/2020_01_13_215342_create_table_user_like.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserLikeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_like', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('recipe_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('recipe_id')->references('id')->on('recipes')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_like');
+    }
+}

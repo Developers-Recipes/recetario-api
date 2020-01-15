5249844bfa2ab9c5a733c53fa7970c7c3d4fe870
Anderson <anderson.ivan.fuentes@gmail.com>
2020-01-14 21:44:08 -0600

    Creating Login Method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\RequestController;
+use App\Http\Requests\AuthLoginRequest;
 use App\Http\Requests\AuthSignUpRequest;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,16 +21,33 @@ class AuthController extends RequestController
 
         $user = User::create($params);
 
-        return $this->showOne($user);
+        return $this->showOne($user, 201);
     }
 
-    public function login()
+    public function login(AuthLoginRequest $request)
     {
-        $email = request('email');
-        $password = request('password');
+        $credentials = request(['email', 'password']);
 
-        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+        if (!Auth::attempt($credentials)) {
+            return $this->errorResponse('Credenciales incorrectas', 401);
         }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me) {
+            $token->expires_at = Carbon::now()->addMonth(1);
+        }
+        $token->save();
+
+        $data = [
+            'access_token' => $tokenResult->accessToken,
+            'token_type'   => 'Bearer',
+            'expires_at'   => Carbon::parse($tokenResult->token->expires_at)
+                ->toDateTimeString()
+        ];
+
+        return response()->json($data);
     }
 
     public function logout()

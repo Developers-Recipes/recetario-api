14f1626780d5e202ea1ad5393029599d27ca7df1
Anderson <anderson.ivan.fuentes@gmail.com>
2020-01-14 23:05:26 -0600

    repair

--- app/Models/Api.php
+++ app/Models/Api.php
@@ -6,13 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Api extends Model
 {
-<<<<<<< HEAD:app/Models/api.php
     protected $fillable = [
         'version',
         'name'
     ];
 }
-=======
-    protected $fillable = ['version','name'];
-}
->>>>>>> 3e09e2183007c97a7fdb05e4154cbb34f1706d79:app/Models/Api.php

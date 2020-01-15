b5856b3d0b1335b03612b7147267374e97948b8b
Anderson <anderson.ivan.fuentes@gmail.com>
2020-01-14 23:41:39 -0600

    Modified models to incorporate softdeletes

--- app/Models/Step.php
+++ app/Models/Step.php
@@ -3,11 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Step extends Model
 {
     const COMPLETED_STEP = true;
     const INCOMPLETED_STEP = false;
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     protected $fillable = [
         'recipe_id',
